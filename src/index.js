45ea2a2c2cfdd71a478f38bd88bd946c3afbe020
KatyaMikanov <kategubko@gmail.com>
2023-01-24 16:07:18 +0300

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let numberAbs = Math.abs(n);
+    let numberToStr = numberAbs.toString().split('').reverse();
+    let resultStr = numberToStr.reduce ((acc, el) => acc + el);
+    let result = Number(resultStr);
+    return result;
 }
